2aa2ec39ec00888325370f7802b39582e8f33f59
Maria Ghorbani <maria.ghrm@gmail.com>
2025-12-10 07:27:02 +0100

    chore: remove unused exports

--- src/components/RoomList/components/index.ts
+++ src/components/RoomList/components/index.ts
@@ -1,6 +1,1 @@
-export { ImageMedia } from './ImageMedia';
-export { ImageSlider } from './ImageSlider';
-export { MediaViewer } from './MediaViewer';
 export { RoomCard } from './RoomCard';
-export { VariantCard } from './VariantCard';
-export { VideoMedia } from './VideoMedia';
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,1 @@
 export * from './roomService';
-export * from './types';
